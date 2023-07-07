a5fbbb95ce910e6eff9e1ebfbacfb65f4ea8ded8
Luke Gartner <lukegartner4@gmail.com>
2023-07-07 10:23:43 -0500

    add MUI app bar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import ButtonAppBar from "../Header/ButtonAppBar";
 import InputPage from "../InputPages/InputPage";
 import Review from "../InputPages/Review";
 import StartPage from "../StartPages/StartPage";
@@ -8,12 +9,7 @@ import StartPage from "../StartPages/StartPage";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4>Don't forget it!</h4>
-        </header>
-      </div>
+      <ButtonAppBar />
       <Route path="/" exact>
         <StartPage page="start" />
       </Route>
