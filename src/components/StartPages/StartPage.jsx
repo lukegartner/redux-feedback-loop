45ec72aef42670615119da201b6d91640e340a82
Luke Gartner <lukegartner4@gmail.com>
2023-07-07 10:55:27 -0500

    style StartPage with MUI

--- src/components/StartPages/StartPage.jsx
+++ src/components/StartPages/StartPage.jsx
@@ -1,6 +1,15 @@
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom/";
 
+// MUI
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { grey } from "@mui/material/colors";
+
 const StartPage = ({ page }) => {
   const history = useHistory();
   const dispatch = useDispatch();
@@ -11,12 +20,26 @@ const StartPage = ({ page }) => {
   };
 
   return (
-    <div>
-      <h2>{page === "complete" ? "Thank You!" : "Welcome"}</h2>
-      <button onClick={start}>
-        {page === "complete" ? "Leave New Feedback" : "Leave Feedback"}
-      </button>
-    </div>
+    <Card
+      sx={{
+        mt: 4,
+        width: "90%",
+        maxWidth: 850,
+        bgcolor: "#eceff1",
+        mx: "auto",
+      }}
+    >
+      <CardContent>
+        <Typography variant="h2" align="center" color="primary.main">
+          {page === "complete" ? "Thank You!" : "Welcome"}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button onClick={start} variant="contained" sx={{ mx: "auto", mb: 2 }}>
+          {page === "complete" ? "Leave New Feedback" : "Leave Feedback"}
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
