68f5ac70157dd68fee290ba4d10d6e12c26838a2
Luke Gartner <lukegartner4@gmail.com>
2023-07-10 12:29:34 -0500

    add Mui list with navigation to display user input

--- src/components/InputPages/Review.jsx
+++ src/components/InputPages/Review.jsx
@@ -1,5 +1,20 @@
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+
+// MUI
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import Divider from "@mui/material/Divider";
+import { ListItemText } from "@mui/material";
+
 const Review = () => {
   const history = useHistory();
   const { feeling, understanding, support, comments } = useSelector(
@@ -16,15 +31,66 @@ const Review = () => {
     history.push("/complete");
   };
   return (
-    <div>
-      <button onClick={() => history.goBack()}>Back</button>
-      <h2>Review Your Feedback:</h2>
-      <p>Feeling: {feeling}</p>
-      <p>Understanding: {understanding}</p>
-      <p>Support: {support}</p>
-      <p>Comments: {comments}</p>
-      <button onClick={postFeedback}>Submit</button>
-    </div>
+    <Card
+      sx={{
+        mt: 4,
+        width: "90%",
+        maxWidth: 850,
+        bgcolor: "#eceff1",
+        mx: "auto",
+      }}
+    >
+      <CardContent>
+        <Typography variant="h4" align="center">
+          Review Your Feedback
+        </Typography>
+        <List></List>
+        <ListItem disablePadding>
+          <ListItemButton onClick={() => history.push("/feeling")}>
+            <ListItemText
+              primary={`Feeling`}
+              secondary={feeling}
+              align="center"
+            >
+              Feeling: {feeling}
+            </ListItemText>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton onClick={() => history.push("/understanding")}>
+            <ListItemText
+              primary="Understanding"
+              secondary={understanding}
+              align="center"
+            ></ListItemText>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton onClick={() => history.push("/support")}>
+            <ListItemText
+              primary="Support"
+              secondary={support}
+              align="center"
+            ></ListItemText>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton onClick={() => history.push("/comments")}>
+            <ListItemText
+              primary="Comments"
+              secondary={comments}
+              align="center"
+            ></ListItemText>
+          </ListItemButton>
+        </ListItem>
+      </CardContent>
+      <CardActions>
+        <Button onClick={() => history.goBack()}>Back</Button>
+        <Button variant="contained" onClick={postFeedback}>
+          Submit
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
